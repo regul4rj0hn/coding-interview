2ed9e0b7289a978e8182a112f41bf77bd3eb81c0
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-01-25 17:07:39 -0300

    csharp validate subsequence

---
+++ easy/validate-subsequence/c-sharp/brute-force.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+/*
+    Time: O(n) - Has to run through the whole list once
+    Space: O(1) - No extra storage
+*/
+public class Program {
+    public static bool IsValidSubsequence (List<int> array, List<int> sequence) {
+        int i = 0, j = 0;
+
+        while (i < array.Count && j < sequence.Count) {
+            if (array[i] == sequence[j]) {
+                j++;
+            }
+            i++;
+        }
+
+        return j == sequence.Count;
+    }
+}
---
+++ easy/validate-subsequence/c-sharp/early-break.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+/*
+    Time: O(n) - Worst case runs through the whole list once
+    Space: O(1) - No extra storage
+*/
+public class Program {
+    public static bool IsValidSubsequence (List<int> array, List<int> sequence) {
+        int i = 0;
+
+        foreach (var n in array) {
+            if (i == sequence.Count) {
+                break;
+            }
+            if (n == sequence[i]) {
+                i++;
+            }
+        }
+
+        return i == sequence.Count;
+    }
+}
