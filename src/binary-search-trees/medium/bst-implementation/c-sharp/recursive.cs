4b53089e10e7f7975c12d43304cce8bf66180739
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-02-15 12:21:58 -0300

    csharp bst implementation remove recursive

--- src/binary-search-trees/medium/bst-implementation/c-sharp/recursive.cs
+++ src/binary-search-trees/medium/bst-implementation/c-sharp/recursive.cs
@@ -20,17 +20,14 @@ public class Program {
                 if (left == null) {
                     BST node = new BST (value);
                     left = node;
-                }
-                else {
+                } else {
                     left.Insert (value);
                 }
-            }
-            else {
+            } else {
                 if (right == null) {
                     BST node = new BST (value);
                     right = node;
-                }
-                else {
+                } else {
                     right.Insert (value);
                 }
             }
@@ -50,18 +47,56 @@ public class Program {
             return false;
         }
 
-        // Grab the smallest, left-most value on the right sub-tree of our target node to replace
+        
         public BST Remove (int value) {
-            Remove(value, null);
+            Remove (value, null);
             return this;
         }
 
+        // helper remove method to keep track of the parent of the current node
         private void Remove (int value, BST parent) {
+            // find the value to remove in the left sub-tree of the current node
             if (value < this.value && left != null) {
                 left.Remove (value, this);
             }
+            // find the value to remove in the right sub-tree of the current node
+            else if (value > this.value && right != null) {
+                right.Remove (value, this);
+            }
+            // the current node value is not less-than or bigger than the target 
+            // if the current node has two children, grab the smallest left-most
+            // value on the right sub-tree and attempt to remove that node
+            else if (left != null && right != null) {
+                this.value = right.GetMinimumValue ();
+                right.Remove (this.value, this);
+            }
+            // root node that has only a left child, shift the left sub-tree up
+            else if (parent == null && left != null) {
+                this.value = left.value;
+                right = left.right;
+                left = left.left;
+            }
+            // if it has only a right child, shift the right sub-tree up
+            else if (right != null) {
+                this.value = right.value;
+                left = right.left;
+                right = right.right;
+            }
+            // if current node is not the root and is the left child of some parent
+            // if the left child of the current node is not null, shift it up and 
+            // assign it to parent.left, otherwise assign right child
+            else if (parent != null && parent.left == this) {
+                parent.left = left != null ? left : right;
+            }
+            // if current node is not the root and is the right child of some parent
+            // if the left child of the current node is not null, shift it up and 
+            // assign it to parent.right, otherwise assign right child
+            else if (parent != null && parent.right == this) {
+                parent.right = left != null ? left : right;
+            }
         }
 
+        // Find the smallest, left-most value of the current sub-tree
         private int GetMinimumValue () {
             return left == null ? value : left.GetMinimumValue ();
         }
