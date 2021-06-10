da982362d430ebb8ad3e7de332bc744e7f9d80d8
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-06-10 11:07:40 -0300

    golang depth first search

---
+++ src/graphs/easy/depth-first-search/go/dfs-recursive.go
@@ -0,0 +1,14 @@
+package main
+
+type Node struct {
+	Name     string
+	Children []*Node
+}
+
+func (n *Node) DepthFirstSearch(array []string) []string {
+	array = append(array, n.Name)
+	for _, child := range n.Children {
+		array = child.DepthFirstSearch(array)
+	}
+	return array
+}
