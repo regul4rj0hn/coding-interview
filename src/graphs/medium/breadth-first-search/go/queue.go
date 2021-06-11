834f1f6ab931a336ec52d2591fb7c8ce14f0bf0c
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-06-11 10:11:34 -0300

    golang breadth first search

---
+++ src/graphs/medium/breadth-first-search/go/queue.go
@@ -0,0 +1,19 @@
+package main
+
+type Node struct {
+	Name     string
+	Children []*Node
+}
+
+func (n *Node) BreadthFirstSearch(array []string) []string {
+	queue := []*Node{n}
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		array = append(array, current.Name)
+		for _, child := range current.Children {
+			queue = append(queue, child)
+		}
+	}
+	return array
+}
