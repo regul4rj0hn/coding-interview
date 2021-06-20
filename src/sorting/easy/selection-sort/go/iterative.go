a44321bf4a2f91da54a93c1a1fe52bcdcc7af196
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-06-20 10:15:11 -0300

    golang selection sort

---
+++ src/sorting/easy/selection-sort/go/iterative.go
@@ -0,0 +1,17 @@
+package main
+
+// Best/Avg/Worst: O(n^2) time | O(1) space
+func SelectionSort(array []int) []int {
+	currentIndex := 0
+	for currentIndex < len(array)-1 {
+		smallestIndex := currentIndex
+		for i := currentIndex + 1; i < len(array); i++ {
+			if array[smallestIndex] > array[i] {
+				smallestIndex = i
+			}
+		}
+		array[currentIndex], array[smallestIndex] = array[smallestIndex], array[currentIndex]
+		currentIndex += 1
+	}
+	return array
+}
