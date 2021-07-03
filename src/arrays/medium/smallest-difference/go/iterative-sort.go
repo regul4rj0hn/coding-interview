981d0a17bc07a9508f8633a6109e704abcd64408
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-07-03 12:20:57 -0300

    golang smallest difference

---
+++ src/arrays/medium/smallest-difference/go/iterative-sort.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+func SmallestDifference(array1, array2 []int) []int {
+	sort.Ints(array1)
+	sort.Ints(array2)
+	idxOne, idxTwo := 0, 0
+	smallest, current := math.MaxInt32, math.MaxInt32
+	smallestPair := []int{}
+	for idxOne < len(array1) && idxTwo < len(array2) {
+		first, second := array1[idxOne], array2[idxTwo]
+		if first < second {
+			current = second - first
+			idxOne += 1
+		} else if first > second {
+			current = first - second
+			idxTwo += 1
+		} else {
+			return []int{first, second}
+		}
+		if smallest > current {
+			smallest = current
+			smallestPair = []int{first, second}
+		}
+	}
+	return smallestPair
+}
