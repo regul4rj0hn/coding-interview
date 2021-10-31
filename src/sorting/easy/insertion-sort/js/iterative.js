10ad617839d42f137029270a796467696a4db4ff
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-10-31 09:46:33 -0300

    js insertion sort

---
+++ src/sorting/easy/insertion-sort/js/iterative.js
@@ -0,0 +1,19 @@
+// O(n^2) time | O(1) space
+function insertionSort(array) {
+	for (let i = 1; i < array.length; i++) {
+		let j = i
+		while (j > 0 && array[j] < array[j - 1]) {
+			swap(j, j - 1, array)
+			j -= 1
+		}
+	}
+	return array
+}
+
+function swap(i,j, array) {
+	const tmp = array[j]
+	array[j] = array[i]
+	array[i] = tmp
+}
+
+exports.insertionSort = insertionSort;
