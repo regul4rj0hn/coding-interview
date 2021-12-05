c806d2b60b516b94ae94de6d517f5b1e147546a1
Mauro Lavigna <mlavigna@intouchhealth.com>
2021-12-05 09:52:36 -0300

    js: graph breadth first search

---
+++ src/graphs/medium/breadth-first-search/js/iterative-queue.js
@@ -0,0 +1,26 @@
+class Node {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(name) {
+        this.children.push(new Node(name));
+        return this;
+    }
+
+    // O(v + e) time | O(v) space
+    breadthFirstSearch(array) {
+        const queue = [this]
+        while (queue.length > 0) {
+            const current = queue.shift()
+            array.push(current.name)
+            for (const child of current.children) {
+                queue.push(child)
+            }
+        }
+        return array
+    }
+}
+
+exports.Node = Node;
