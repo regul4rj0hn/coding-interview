1d6b0402aa696eeafd6ed01f2a72ff7a8f27f400
Mauro Lavigna <mauro.lavigna@gmail.com>
2021-12-11 10:10:11 -0300

    js: number of ways to make change dp

---
+++ src/dynamic-programming/medium/ways-to-make-change/js/iterative.js
@@ -0,0 +1,15 @@
+// O(n.d) time | O(n) space
+function numberOfWaysToMakeChange(n, denoms) {
+    const ways = new Array(n + 1).fill(0)
+    ways[0] = 1
+    for (let denom of denoms) {
+        for (let amount = 1; amount < n + 1; amount++) {
+            if (denom <= amount) {
+                ways[amount] += ways[amount - denom]
+            }
+        }
+    }
+    return ways[n]
+}
+
+exports.numberOfWaysToMakeChange = numberOfWaysToMakeChange;
